4cf8cb4314c06efd33a45e0adf88401b33c35dea
Frkty22 <frkty22@gmail.com>
2025-03-17 02:00:22 +0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,18 +1,2 @@
 #!/bin/bash
-apt-get update && apt-get install -y \
-  chromium \
-  libx11-xcb1 \
-  libxcomposite1 \
-  libxdamage1 \
-  libxi6 \
-  libxtst6 \
-  libnss3 \
-  libxrandr2 \
-  libasound2 \
-  libpangocairo-1.0-0 \
-  libatk1.0-0 \
-  libatk-bridge2.0-0 \
-  libgtk-3-0 \
-  libgbm1 \
-  libxshmfence1
 npm install
